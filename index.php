236d6d98150219bf6cdde86c7e72142692e1cc10
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-13 01:57:41 -0500

    p1.loc ready for production

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
 
 </head>
 <body>
-
-    <h1>My New Website</h1>
-    <img src='./images/picture.jpg' alt='A Photo of Something'/>
-
-    <div id='quotes'>
-        <?php include "./php/quotes.php"; ?>
+    <div id='content_border'>
+        <div id='content'>
+            <div id='title'><span class='center-text'>Jeremy C. Southgate</span></div>
+            <img id='portrait' src='./images/portrait.jpg' alt='Portrait'/>
+            <div id='quotes'>
+                <h1 class='center-text'>&ldquo;<?php include "./php/quotes.php"; ?>&rdquo;</h1>
+            </div>
+        </div>
     </div>
-
 </body>
 </html>
--- php/quotes.php
+++ php/quotes.php
@@ -1,5 +1,30 @@
 <?php
 
-    
+    // A PHP Script to return a random quotes
+
+
+    // Set possible quotes (strings) as variables here:
+
+    $quote1 = "I speak English and German.";
+
+    $quote2 = "I'm a Singer and Composing Musician.";
+
+    $quote3 = "I'm proud to be from Massachusetts.";
+
+    $quote4 = "I study Computer Science at Harvard.";
+
+    $quote5 = "I'm a Saggitarius.";
+
+
+    // Create an array of the variables
+    $array = array($quote1, $quote2, $quote3, $quote4, $quote5);
+
+
+    // Generate a random integer between 0 and 4 (to rand. select var. from array)
+    $random_int = rand(0,4);
+
+
+    // Return the element of the array which is at the random_int position
+    echo $array[$random_int];
 
 ?>
