ff2f50f67c3399a5a1c01ece2af0eaad0727ed75
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-13 13:13:01 -0500

    Checked code style, added code comments

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!doctype html>
 <html lang='en'>
 <head>
+
+    <!-- HTML Required Headers -->
     <title>DWA Project 1</title>
     <meta charset="utf-8" />
+
+    <!-- Stylesheet for this Page -->
     <link rel='stylesheet' href='./styles/index.css'/>
+
 </head>
 <body>
+
+    <!-- Double rounded rectangles form a content area -->
     <div id='content_border'>
         <div id='content'>
+
+            <!-- Page Heading/Title -->
             <div id='title'><span class='center-text'>Jeremy C. Southgate</span></div>
+
+            <!-- Personal Portrait -->
             <img id='portrait' src='./images/portrait.jpg' alt='Portrait'/>
+
+            <!-- Personal Description Paragraph -->
             <div id='description'>
                 <p class='center-text'>
                     I am a degree candidate for Bachelor of Liberal Arts at Harvard Extension School.
@@ -21,10 +34,14 @@
                     rowing on the Charles!
                 </p>
             </div>
+
+            <!-- Dynamically-generated quotes -->
             <div id='quotes'>
                 <h1 class='center-text'>&ldquo;<?php include "./php/quotes.php"; ?>&rdquo;</h1>
             </div>
+
         </div>
     </div>
+
 </body>
 </html>
