cf04d3c0a05c01a45159c63551b62aac29d71931
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-02-13 02:30:58 -0500

    added a paragraph element

--- index.php
+++ index.php
@@ -12,6 +12,17 @@
         <div id='content'>
             <div id='title'><span class='center-text'>Jeremy C. Southgate</span></div>
             <img id='portrait' src='./images/portrait.jpg' alt='Portrait'/>
+            <div id='description'>
+                <p class='center-text'>
+                    I am a degree candidate for Bachelor of Liberal Arts at Harvard Extension School.
+                    To date, I have taken EXPO E-25 Academic Writing and Critical Reading,
+                    DGMD E-50 Visual Communication Design, CSCI E-50 Intensive Intro to Computer Science,
+                    MATH E-3 Quantitative Reasoning, EXPO E-34 Business Rhetoric. In addition to this course
+                    this semester I am taking GOVT E-1045 Justice, CSCI E-31 Javascript with Node.js, and
+                    DGMD E-37 Motion Graphics. I enjoy reading in Widener, swimming in Blodgett pool, and
+                    rowing on the Charles!
+                </p>
+            </div>
             <div id='quotes'>
                 <h1 class='center-text'>&ldquo;<?php include "./php/quotes.php"; ?>&rdquo;</h1>
             </div>
